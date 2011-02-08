c7e68659e8769f5758a5a49c002dee452fd78661
Jonathan Swartz <swartz@pobox.com>
2011-02-08 14:36:42 -0800

    init

--- lib/Mason/Plugin/HTMLFilters.pm
+++ lib/Mason/Plugin/HTMLFilters.pm
@@ -1,24 +1,49 @@
 package Mason::Plugin::HTMLFilters;
-use 5.008;
-use strict;
-use warnings;
+use Moose;
+with 'Mason::Plugin';
 
 1;
 
-# ABSTRACT: My great module
+# ABSTRACT: HTML filters
 __END__
-
-=pod
-
-=head1 SYNOPSIS
-
-    use Mason::Plugin::HTMLFilters;
 
 =head1 DESCRIPTION
 
-Mason::Plugin::HTMLFilters provides
+Filters related to HTML generation.
+
+=head1 FILTERS
+
+=over
+
+=item HTML or H
+
+Do a basic HTML escape on the content - just the characters '&', '>', '<', and
+'"'.
+
+=item HTMLEntities
+
+Do a comprehensive HTML escape on the content, using
+HTML::Entities::encode_entities.
+
+=item URI
+
+URI-escape the content.
+
+=item HTMLPara
+
+Formats a block of text into HTML paragraphs.  A sequence of two or more
+newlines is used as the delimiter for paragraphs which are then wrapped in HTML
+""<p>""...""</p>"" tags. Taken from L<Template::Toolkit|Template>.
+
+=item HTMLParaBreak
+
+Similar to HTMLPara above, but uses the HTML tag sequence "<br><br>" to join
+paragraphs. Taken from L<Template::Toolkit|Template>.
+
+=back
 
 =head1 SEE ALSO
 
-L<Some::Module>
+L<Mason|Mason>
 
+=cut
--- t/Sanity.t
+++
@@ -1,3 +0,0 @@
-#!perl -w
-use Mason::Plugin::HTMLFilters::t::Sanity;
-Mason::Plugin::HTMLFilters::t::Sanity->runtests;
