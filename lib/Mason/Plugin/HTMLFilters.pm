f73e541fd18f804c355056cc3cd176a469f0ff3f
Jonathan Swartz <swartz@pobox.com>
2011-02-21 18:20:42 -0500

    add support section

--- lib/Mason/Plugin/HTMLFilters.pm
+++ lib/Mason/Plugin/HTMLFilters.pm
@@ -73,8 +73,26 @@ outputs:
 
 =back
 
+=head1 SUPPORT
+
+The mailing list for Mason and Mason plugins is
+L<mason-users@lists.sourceforge.net>. You must be subscribed to send a message.
+To subscribe, visit
+L<https://lists.sourceforge.net/lists/listinfo/mason-users>.
+
+You can also visit us at C<#mason> on L<irc://irc.perl.org/#mason>.
+
+Bugs and feature requests will be tracked at RT:
+
+    http://rt.cpan.org/NoAuth/Bugs.html?Dist=Mason-Plugin-HTMLFilters
+    bug-mason-plugin-htmlfilters@rt.cpan.org
+
+The latest source code can be browsed and fetched at:
+
+    http://github.com/jonswar/perl-mason-plugin-htmlfilters
+    git clone git://github.com/jonswar/perl-mason-plugin-htmlfilters.git
+
 =head1 SEE ALSO
 
 L<Mason|Mason>
 
-=cut
